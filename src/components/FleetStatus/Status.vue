fed15996a571da2825508d69f27da09d7ee320b2
Florence Diep <florence.diep@mail.mcgill.ca>
2018-11-18 01:34:44 -0500

    renamed files, added buttons and paths

---
+++ src/components/FleetStatus/Status.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fleet_status">
+    <h2>Fleet Status</h2>
+    <div style="padding-top: 20px;">
+      <b-button-group vertical size="lg">
+        <b-button href="#/status/drivers">Drivers</b-button>
+        <b-button href="#/status/passengers">Passengers</b-button>
+        <b-button href="#/status/journeys/">Journeys</b-button>
+      </b-button-group>
+  </div>
+    <div style="padding-top: 50px;">
+      <b-button-group vertical size="lg">
+        <b-button href="#/">Home</b-button>
+      </b-button-group>
+  </div>
+  </div>
+</template>
+
+<script src="./status.js">
+</script>
+
+<style>
+  #ridesharingapp {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <div style="padding-top: 20px;">
+      <b-button-group vertical size="lg">
+        <b-button href="#/status">Fleet Status</b-button>
+        <b-button href="#/rankings">Rankings</b-button>
+      </b-button-group>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
