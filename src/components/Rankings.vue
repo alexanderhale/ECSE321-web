b4e61a8543a91106b5b7d01403b50c4ee2e3c2fb
Adam Gobran <adamgobes@gmail.com>
2018-11-22 13:16:32 -0500

    load journeys into state

--- src/components/Rankings.vue
+++ src/components/Rankings.vue
@@ -13,15 +13,15 @@
 		<b-container fluid class="filter-inputs">
 			<b-row class="input">
 				<b-col sm="3"><label for="start-date">Start date:</label></b-col>
-      			<b-col sm="9"><b-form-input id="start-date" type="date"></b-form-input></b-col>
+      			<b-col sm="9"><b-form-input id="start-date" type="date" v-model="startDateFilter" v-on:change="updateJourneys()"></b-form-input></b-col>
 			</b-row>
 			<b-row class="input">
 				<b-col sm="3"><label for="end-date">End date:</label></b-col>
-      			<b-col sm="9"><b-form-input id="end-date" type="date"></b-form-input></b-col>
+      			<b-col sm="9"><b-form-input id="end-date" type="date" v-model="endDateFilter" v-on:change="updateJourneys()"></b-form-input></b-col>
 			</b-row>
 		</b-container>
-		<b-table class="ranking-table" striped hover :items="items"></b-table>
-	</div>
+		<b-table class="ranking-table" striped hover :items="filteredJourneys ? filteredJourneys : journeys"></b-table>
+	</div>	
 	<div style="padding-top: 20px;">
       <b-button-group vertical size="lg">
         <b-button v-on:click="resetCategory()" v-if="selectedCategory">Back</b-button>
@@ -39,7 +39,8 @@
     color: #2c3e50;
     background: #f2ece8;
 }
-.ranking-buttons, .ranking-info {
+.ranking-buttons,
+.ranking-info {
     margin-top: 20px;
     display: flex;
     flex-direction: column;
@@ -54,16 +55,16 @@
 }
 
 .filter-inputs {
-	width: 600px;
-	margin-top: 20px;
+    width: 600px;
+    margin-top: 20px;
 }
 
 .input {
-	margin: 20px 0;
+    margin: 20px 0;
 }
 
 .ranking-table {
-	margin-top: 32px;
-	width: 80%;
+    margin-top: 32px;
+    width: 80%;
 }
 </style>
--- src/components/rank.js
+++ src/components/rank.js
@@ -1,30 +1,70 @@
 import { baseUrl } from '../apiConfig';
 import axios from 'axios';
+import moment from 'moment';
 
 export default {
-	name: 'rankings',
-	data() {
-		return {
-			selectedCategory: '',
-			journeys: []
-		}
-	},
-	methods: {
-		setCategory(val) {
-			this.selectedCategory = val
-		},
+  name: 'rankings',
+  data() {
+    return {
+      selectedCategory: '',
+      journeys: [],
+      filteredJourneys: null,
+      startDateFilter: '',
+      endDateFilter: ''
+    };
+  },
+  methods: {
+    setCategory(val) {
+      this.selectedCategory = val;
+    },
 
-		resetCategory() {
-			this.selectedCategory = null;
-		} 
-	},
-	mounted () {
-		axios.get(`${baseUrl}/journey/all`, {
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-			.then(res => this.journeys = res.data)
-			.catch(err => console.error(err))
-	  }
-}
+    resetCategory() {
+      this.selectedCategory = null;
+    },
+
+    updateJourneys() {
+      const startDate = moment(this.startDateFilter);
+      const endDate = moment(this.endDateFilter);
+      if (!startDate._isValid && !endDate._isValid)
+        this.filteredJourneys = null;
+      else {
+        this.filteredJourneys = this.journeys.filter(j =>
+          this.startDateFilter && this.endDateFilter
+            ? moment(j.date, 'YYYY-MM-DD').isBetween(startDate, endDate)
+            : true
+        );
+      }
+    }
+  },
+  mounted() {
+    axios
+      .get(`${baseUrl}/journey/all`, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(
+        res =>
+          (this.journeys = res.data
+            .map(journey => {
+              return {
+                startAddress: journey.startAddress,
+                startCity: journey.startCity,
+                startCountry: journey.startCountry,
+                endAddress: journey.endAddress,
+                endCity: journey.endCity,
+                endCountry: journey.endCountry,
+                rating: journey.rating,
+                date: moment(
+                  journey.timePickup.substring(
+                    0,
+                    journey.timePickup.indexOf('-')
+                  )
+                ).format('YYYY-MM-DD')
+              };
+            })
+            .sort((a, b) => b.rating - a.rating))
+      )
+      .catch(err => console.error(err));
+  }
+};
